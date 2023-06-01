3e7393664e7bf165c94e5c7771189ab764db0bca
aidanjrauscher <ajrscomputer@gmail.com>
2023-06-01 14:55:07 -0400

    m

--- random-generator/pcconfig.py
+++ random-generator/pcconfig.py
@@ -3,8 +3,9 @@ import pynecone as pc
 class RandomgeneratorConfig(pc.Config):
     pass
 
-config = RandomgeneratorConfig(
+config = pc.Config(
     app_name="random_generator",
+    api_url="0.0.0.0:8000",
+    bun_path="/app/.bun/bin/bun",
     db_url="sqlite:///pynecone.db",
-    env=pc.Env.DEV,
 )
